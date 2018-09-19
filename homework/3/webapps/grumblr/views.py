c71ff1d0179ec9438257b6f337dd82404cd7af1e
Lyuliang Liu <lyuliangliu@LyuliangdeMBP.wv.cc.cmu.edu>
2018-09-19 12:05:57 -0400

    modified some redirections of grumblr

--- homework/3/webapps/grumblr/views.py
+++ homework/3/webapps/grumblr/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.decorators import login_required
+import datetime
 
 from grumblr.models import *
 
