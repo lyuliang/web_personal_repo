a89102e725fde2493884508fa6d57f8df3cf2e5c
Lyuliang Liu <lyuliangliu@LyuliangdeMBP.wv.cc.cmu.edu>
2018-10-08 14:32:50 -0400

    finish inclass 12

--- in-class/12 Ajax/fortune.js
+++ in-class/12 Ajax/fortune.js
@@ -1,5 +1,20 @@
-
+var req;
 function getFortune() {
 
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+    } else {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    req.onreadystatechange = handleResponse;
+    req.open("GET", "https://garrod.isri.cmu.edu/webapps/fortune/", true);
+    req.send();
 }
 
+function handleResponse() {
+    if (req.readyState != 4 || req.status != 200) {
+        return;
+    }
+    var x = document.getElementById("content");
+    x.innerHTML = this.responseText;
+}
