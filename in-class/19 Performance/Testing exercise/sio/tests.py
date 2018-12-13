f68f4aa2f11b6723e1cabeff6f71dab505dd4c55
Lyuliang Liu <lyuliangliu@LyuliangdeMacBook-Pro.local>
2018-12-12 21:47:38 -0500

    finished inclass 19

--- in-class/19 Performance/Testing exercise/sio/tests.py
+++ in-class/19 Performance/Testing exercise/sio/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from django.test.client import *
+from django.shortcuts import reverse
 # Create your tests here.
+client = Client()
+response1 = client.post(reverse('create_course'), {'course_number':'001', 'course_name':'math', 'instructor':'Mike'})
+response2 = client.post(reverse('create_student'), {'andrew_id':'lyulianl', 'first_name':'Lyuliang', 'last_name':'Liu'})
